57427316794d8fc656111557fec9ca214e6af075
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-06-27 17:36:58 +0530

    feat:- use of Debouncing technique

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import hamburger from "../assests/hamburger.svg";
 import youtube from "../assests/youtube.png";
 import profile from "../assests/profile.png";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constant";
+import { json } from "react-router-dom";
 const Head = () => {
-    const dispath = useDispatch()
+  const [searchQuery, setSearchQuery] = useState("");
+  const [susggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      youtubeSearchSuggestions();
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const youtubeSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    console.log(searchQuery);
+
+    const json = await data.json();
+    console.log(json[1]);
+    setSuggestions(json[1]);
+  };
+  const dispath = useDispatch("");
   const toggleMenuHandler = () => {
-    dispath(toggleMenu())
+    dispath(toggleMenu());
   };
   return (
     <div className=" grid grid-flow-col p-5 m-1 shadow-lg ">
@@ -19,20 +41,33 @@ const Head = () => {
           src={hamburger}
         />
         <a href="">
-        <img className=" h-12 mx-3 " alt="youtube-logo" src={youtube} />
+          <img className=" h-12 mx-3 " alt="youtube-logo" src={youtube} />
         </a>
       </div>
-      <div className=" col-span-10 ">
-        <input
-          className="w-1/2 text-center border border-gray-600 p-2 rounded-l-full "
-          type="text"
-        />
-        <button className=" border border-gray-500 px-7 py-2 rounded-r-full bg-gray-100 ">
-          🔍
-        </button>
+      <div className=" col-span-10 px-10 ">
+        <div>
+          <input
+            className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full "
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button className=" border border-gray-500 px-7 py-2 rounded-r-full bg-gray-100 ">
+            🔍
+          </button>
+        </div>
+        <div className=" fixed bg-white py-2 px-2 w-[27rem] shadow-lg rounded-lg border border-gray-100 ">
+          <ul>
+            {susggestions.map(s => <li key={s} className=" py-2 px-3 shadow-sm hover:bg-gray-400 ">
+              🔍{s}
+            </li>)}
+           
+            
+          </ul>
+        </div>
       </div>
-      <div className="h-10 w-10 col-span-1 ">
-        <img alt="user-icon" src={profile} />
+      <div className=" col-span-1 ">
+        <img className="h-10" alt="user-icon" src={profile} />
       </div>
     </div>
   );
