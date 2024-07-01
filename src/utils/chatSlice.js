985c296456ff1816054ac0439dbefd712ac056f2
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-01 14:12:28 +0530

    feat: here we are deleting every 10 message comment after 1 comment deleted

--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -8,7 +8,8 @@ const chatSlice = createSlice({
 
   reducers: {
     addMessage: (state, action) => {
-      state.messages.push(action.payload);
+      state.messages.splice(10, 1);
+      state.messages.unshift(action.payload);
     },
   },
 });
