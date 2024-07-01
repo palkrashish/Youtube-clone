fb498809c25f264e13a93242389ef6b51e7c51eb
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-01 14:57:38 +0530

    feat:- crete button and some css

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -24,10 +24,15 @@ const LiveChat = () => {
   return (
     <>
       <div className=" w-full h-[600px] ml-1 p-1 border border-black bg-slate-100 rounded-2xl overflow-y-scroll flex flex-col-reverse ">
-        {chatMessage.map((c, index) =>(
+        <div>
+          {chatMessage.map((c, index) => (
             <ChatMessage key={index} name={c.name} message={c.message} />
-        )
-        )}
+          ))}
+        </div>
+      </div>
+      <div className="w-full p-2 ml-2 mt-0 border border-black ">
+        <input className="w-90" type="text" />
+        <button className="px-2 mx-2 bg-green-100">Send</button>
       </div>
     </>
   );
--- src/utils/chatSlice.js
+++ src/utils/chatSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { OFFSET_LIVE_CHAT } from "./constant";
 
 const chatSlice = createSlice({
   name: "chat",
@@ -8,7 +9,7 @@ const chatSlice = createSlice({
 
   reducers: {
     addMessage: (state, action) => {
-      state.messages.splice(10, 1);
+      state.messages.splice(OFFSET_LIVE_CHAT, 1);
       state.messages.unshift(action.payload);
     },
   },
